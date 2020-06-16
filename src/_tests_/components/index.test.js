af3699445334cacc3418ae6a88d8107b08f2b08b
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-16 17:30:41 -0500

    finally got first test working

---
+++ src/_tests_/components/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import Footer from '../../components/footer'
+
+describe('<Footer />', () => {
+    it('can render footer', () => {
+        let app = shallow(<Footer />);
+        expect(app.find('span').text()).toBe('2020 DeltaV')
+    })
+})
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 2018 Code Fellows</footer>;
+    return <footer>&copy;<span>2020 DeltaV</span> </footer>;
   }
 }
 
