ac701ee0bfb5d570ec487fcb46d5f78a976ff2d9
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-16 20:30:31 -0500

    changed form.js to index.js to simpify importing

--- src/_tests_/components/form.test.js
+++ src/_tests_/components/form.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
-
-import Form from '../../components/form/'
+import {shallow} from 'enzyme';
+import Form from '../../components/form';
 
 describe('<Form />', () => {
-    it('can render button', ()=> {
-        let app = shallow('<Form />');
-        expect(app.find('button')).ToBeTruthy
+    it('can render button', () => {
+        let app = shallow(<Form />);
+        expect(app.find('button').text()).toBe('GO!');
     });
-}) ;
+});
--- src/_tests_/components/index.test.js
+++ src/_tests_/components/index.test.js
@@ -6,6 +6,6 @@ import Footer from '../../components/footer'
 describe('<Footer />', () => {
     it('can render footer', () => {
         let app = shallow(<Footer />);
-        expect(app.find('span').text()).toBe('2020 DeltaV')
-    })
-})
+        expect(app.find('span').text()).toBe('2020 DeltaV');
+    });
+});
