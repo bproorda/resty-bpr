07c30c4a9917222c2469d3c872c84a810088fe53
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-19 19:33:16 -0500

    refatored index.js to use browserRouter

--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,10 @@ import './app.scss';
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form/';
-import Results from './components/Results';
+import Header from './header';
+import Footer from './footer';
+import Form from './form';
+import Results from './Results';
 
 
 class App extends React.Component {
