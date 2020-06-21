a00e4eb0c75141e6595fab820f35b3386adfab6b
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-06-20 21:32:25 -0500

    added close button, no functionality yet

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -5,13 +5,13 @@ import ReactJson from 'react-json-view'
 const Modal = props => {
     let thisEntry = props.location.state.entry;
     let thisId = props.match.params.id;
-    // console.log(thisEntry);
     return (
         <>
             <h2>Results for Request #{thisId} </h2>
             <span>
                 <ReactJson src={thisEntry} theme="monokai" />
             </span>
+            <button >Click here to close</button>
         </>
 
 
